88ba114b5a50a7b991307a01d43642f73d43cffb
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-01-30 18:19:02 +0900

    handle X13Error and refactor

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from pathlib import Path
+import re
+import traceback
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 from korean_romanizer.romanizer import Romanizer
+from statsmodels.tsa.x13 import X13Error
 
 import data
 import x13
+
 
 plt.style.use("./auri.mplstyle")
 
@@ -30,8 +34,9 @@ def run_x13(s: pd.Series, name=None, x12path="./x13as/x13as.exe"):
     s_new = s_new.fillna(0)
 
     try:
-    results = x13.x13_arima_analysis(s_new, x12path=x12path)
-    # return results
+        results = x13.x13_arima_analysis(s_new, x12path=x12path)
+    except X13Error as e:
+        raise e
 
     arima_results = x13.get_arima_order_from_results(results)
     order, sorder = arima_results.order, arima_results.sorder
@@ -48,7 +53,6 @@ def run_x13(s: pd.Series, name=None, x12path="./x13as/x13as.exe"):
     )
 
     fig = results.plot()
-
     return df_result, fig, order, sorder
 
 
@@ -59,10 +63,25 @@ dfs_approval = data.process_data()
 for key, df in dfs_approval.items():
     for label, s in df.items():
         name = "_".join([key, *s.name])
+        # sanitize name
+        name = re.sub("\\W", "_", name)
+
+        print(f"{name}...", end=" ")
 
         if not Path(f"output/{name}.csv").exists():
-            df_result, fig, order, sorder = run_x13(s, name)
+            try:
+                df_result, fig, order, sorder = run_x13(s, name)
 
-            df_result.to_csv(f"output/{name}.csv", encoding="utf-8-sig")
-            fig.savefig(f"output/{name}.png")
-            fig.savefig(f"output/{name}.svg")
+                df_result.to_csv(f"output/{name}.csv", encoding="utf-8-sig")
+                fig.savefig(f"output/{name}.png")
+                fig.savefig(f"output/{name}.svg")
+
+            except X13Error as e:
+                print("error")
+                Path("error").mkdir(exist_ok=True)
+                with open(f"error/{name}.txt", "w") as f:
+                    traceback.print_exc(file=f)
+            else:
+                print("done")
+        else:
+            print("already exists")
